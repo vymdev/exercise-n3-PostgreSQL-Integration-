c01f49b30e89554605d672a65a9e64bc65995e79
vymdev <inversion83101229@gmail.com>
2026-04-20 22:08:30 -0400

    feat(controller): RestControllers API for interacting with products

--- src/main/java/com/example/demo/Controllers/RestControllers.java
+++ src/main/java/com/example/demo/Controllers/RestControllers.java
@@ -1,10 +1,23 @@
 package com.example.demo.Controllers;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.Database;
 import com.example.demo.DatabaseRepository;
 
+import jakarta.validation.Valid;
+
+
+
 @RestController
+@RequestMapping("/products")
 public class RestControllers {
     
     private DatabaseRepository repository;
@@ -12,6 +25,20 @@ public class RestControllers {
     @Autowired
     public RestControllers(DatabaseRepository repository) {
         this.repository = repository;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addProduct(@Valid @RequestBody DatabaseDTO entity) {
+        Database db = new Database();
+        db.setProduct(entity.getProduct());
+        db.setValue(entity.getValue());
+        repository.save(db);
+        return ResponseEntity.ok("product saved!");
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<Database>> viewAllProducts() {
+        return ResponseEntity.ok(repository.findAll());
     }
     
 }
