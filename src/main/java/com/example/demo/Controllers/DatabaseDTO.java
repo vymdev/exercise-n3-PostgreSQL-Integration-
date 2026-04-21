64922c8f0a5b4f6ae9328f1f51592bb315e61751
vymdev <inversion83101229@gmail.com>
2026-04-20 21:54:02 -0400

    feat(dto): add validations to DatabaseDTO

--- src/main/java/com/example/demo/Controllers/DatabaseDTO.java
+++ src/main/java/com/example/demo/Controllers/DatabaseDTO.java
@@ -1,13 +1,22 @@
 package com.example.demo.Controllers;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class DatabaseDTO {
-    
+
+    @NotBlank
+    @Size(min = 3, max = 18)
     private String product;
+
+    @Min(1)
+    @Max(10000)
     private int value;
     
 }
