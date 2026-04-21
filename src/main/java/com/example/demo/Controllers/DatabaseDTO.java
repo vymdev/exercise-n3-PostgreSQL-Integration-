7cbb60641b58ed98d59abbfe96abb95c0b01d1ac
vymdev <inversion83101229@gmail.com>
2026-04-20 21:38:19 -0400

    feat(database): add DTO for Database

---
+++ src/main/java/com/example/demo/Controllers/DatabaseDTO.java
@@ -0,0 +1,13 @@
+package com.example.demo.Controllers;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DatabaseDTO {
+    
+    private String product;
+    private int value;
+    
+}
